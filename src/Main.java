8dbc9cc24ccff2495e6b55eb0f7c209e2cf168db
JavaUsedBeOne <kisarevna@yandex.ru>
2022-11-01 21:21:14 +0800

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,29 @@
 public class Main {
 
     public class SalesManager {
-        protected int[] sales;
+        protected long[] sales;
 
-        public SalesManager(int[] sales) {
+        public SalesManager(long[] sales) {
             this.sales = sales;
         }
 
-        public int max() {
-            int max = -1;
-            for (int sale : sales) {
+        public long max() {
+            long max = -1;
+            for (long sale : sales) {
                 if (sale > max) {
                     max = sale;
                 }
             }
             return max;
+        }
+        public long max1() {
+            long max1 = -1;
+            for (long sale : sales) {
+                if (sale > max1) {
+                    max1 = sale;
+                }
+            }
+            return max1;
         }
     }
 }
